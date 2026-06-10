f95170d720c3987382f28467b9140e338c996192
vctseo <46834499+vctseo@users.noreply.github.com>
2026-06-11 00:06:19 +0700

    fix: set correct base path for GitHub Pages

--- vite.config.github.ts
+++ vite.config.github.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  base: "/",
+  base: "/code-script-gtm-webscon-premium/",
   plugins: [
     react(),
     tailwindcss(),
